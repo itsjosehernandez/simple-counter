37601458bdae69c2c63b4c483bb95714d17ac74a
itsjosehernandez <joseher2702@gmail.com>
2023-04-30 00:28:30 +0000

    Proyecto al repositorio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,40 @@
 import React from "react";
+import "../../styles/index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+
+
+const Home = ({first, second, third, fourth, five, six}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	
+   		<div className="card text-bg-dark mb-3" >
+
+     <div className="card-body">
+       <h5 className="card-title">Temporizador</h5>
+       <p className="card-text"></p>
+		<div class="d-flex flex-row mb-3 ">
+
+	  <div className="cuadrado text-center py-2"><i class="fa-regular fa-clock icon"></i></div>
+	  <div className="cuadrado text-center  py-2 ">{six}</div>
+	  <div className="cuadrado text-center  py-2">{five}</div>
+	  <div className="cuadrado text-center  py-2">{fourth}</div>
+	  <div className="cuadrado text-center  py-2">{third}</div>
+	  <div className="cuadrado text-center  py-2">{second}</div>
+	  <div className="cuadrado text-center  py-2">{first}</div>
+
+
+   	  
+   	  
+   	  
+   	  
+		 </div>
+     </div>
+   </div>
+
+
+
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,24 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+
+setInterval(() => {
+   counter = counter + 1;
+   const firstNumber = Math.floor(counter % 10);
+   const second = Math.floor((counter / 10) % 10);
+   const third = Math.floor((counter / 100) % 10);
+   const fourth = Math.floor((counter / 1000) % 10);
+   const five = Math.floor((counter / 10000) % 10);
+   const six = Math.floor((counter / 10000) % 10);
+
+
+
+   console.log(firstNumber, second, third, fourth)
+
+ 
+
+ReactDOM.render(<Home first={firstNumber} second={second} third={third} fourth={fourth} five={five} six={six} />, document.querySelector("#app"))}, 100);
